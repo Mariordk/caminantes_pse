ef6c3455b033759841585f2ea4ae370b605b132c
Mario <Mario@MARIO>
2016-05-23 03:27:58 +0200

    Ya no hay problema con los beans, pero sigue sin guardar en la bbdd.

---
+++ src/main/java/com/mycompany/caminantes/usuarios/usuariosBackingBean.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.caminantes.usuarios;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+
+/**
+ *
+ * @author Mario
+ */
+
+@Named
+@SessionScoped
+public class usuariosBackingBean implements Serializable{
+    
+    int idUsuario;
+    String nombreUsuario;
+    String password;
+    String nombre;
+    String apellidos;
+    int edad;    
+    String provincia;
+    String rol;
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
+
---
+++ src/main/java/com/mycompany/caminantes/usuarios/usuariosClientBean.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.caminantes.usuarios;
+
+import com.mycompany.caminantes.entities.Usuarios;
+import com.mycompany.caminantes.json.UsuarioReader;
+import com.mycompany.caminantes.json.UsuarioWriter;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+/**
+ *
+ * @author Mario
+ */
+
+@Named
+@RequestScoped
+public class usuariosClientBean {
+    @Inject
+    usuariosBackingBean bean;
+    
+    Client client;
+    WebTarget target;
+    
+    @PostConstruct
+    public void init() {
+        client = ClientBuilder.newClient();
+        target = client.target("http://localhost:8080/cines2015/webresources/com.mycompany.caminantes.entities.usuarios");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        client.close();
+    }
+
+     public Usuarios[] getUsuarios() {
+            return target
+                    .request()
+                    .get(Usuarios[].class);
+    }
+     
+     public Usuarios getUsuario()
+     {
+        /* Usuarios u = target
+                .path("{idUsuario}")
+                .resolveTemplate("idUsuario", bean.getIdUsuario())
+                .request(MediaType.APPLICATION_JSON)
+                .get(Usuarios.class);
+        return u;*/
+         return target
+            .register(UsuarioReader.class)
+            .path("{idUsuario}")
+            .resolveTemplate("idUsuario", bean.getIdUsuario())
+            .request(MediaType.APPLICATION_JSON)
+            .get(Usuarios.class);
+     }
+
+     public void deleteUsuario() {
+        target.path("{idUsuario}")
+                .resolveTemplate("idUsuario", bean.getIdUsuario())
+                .request()
+                .delete();
+    }
+     
+     public void addUsuario() {
+        Usuarios u = new Usuarios();
+        u.setIdUsuario(1);
+        u.setNombreUsuario(bean.getNombreUsuario());
+        u.setPassword(bean.getPassword());
+        u.setNombre(bean.getNombre());
+        u.setApellidos(bean.getApellidos());
+        u.setEdad(bean.getEdad());
+        u.setProvincia(bean.getProvincia());
+        u.setRol(bean.getRol());
+        target.register(UsuarioWriter.class)
+                .request()
+                .post(Entity.entity(u, MediaType.APPLICATION_JSON));
+    }
+}
